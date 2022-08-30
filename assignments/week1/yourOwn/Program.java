53f103f58e9d322ba54d9900f140f7407d467e0b
angela bereski <angelakbereski@gmail.com>
2022-08-30 14:17:35 -0600

    yourOwnProgram

---
+++ assignments/week1/yourOwn/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("My name is Angela Bereski");
+        System.out.println("I am 35 years old");
+        System.out.println("My hometown is Reading, PA");
+    }
+}
